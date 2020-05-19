598cff26f6fd3750f7e9178627b9833cbc3d9faf
Miguel Martínez de Paz (vassmmartinez) <mmartinez_vass@bankinter.com>
2020-05-19 13:58:20 +0200

    micro

---
+++ src/main/java/es/peichman/jos/samples/micro/MicroPrometheusApplication.java
@@ -0,0 +1,12 @@
+package es.peichman.jos.samples.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPrometheusApplication.class, args);
+	}
+}
---
+++ src/test/java/es/peichman/jos/samples/micro/MicroPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package es.peichman.jos.samples.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
